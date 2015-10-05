acaf1950aaa2a39d833607f253622cae3807b25c
SaiNadh <saibaba.nadh@dreamorbit.com>
2015-10-05 18:06:40 +0530

    modified

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var app = express(),
 	port = process.env.PORT || 3000;
 
 app.use(express.static('.'));
-
 app.use(helmet.csp({
   defaultSrc: ["'self'"],
   scriptSrc: ['*.google-analytics.com'],
@@ -24,17 +23,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.use(function(req,res, next){
+	console.log(new Date());
+	var result = authenticate(req.body);
+	if(result)
+		next();
+});
+
 
 app.get('/', function(req,res){
 	res.redirect('index.html');
 });
 
 app.post('/auth/validEmail', function(req,res){
-
 	var email = sanitizer.sanitize(req.body.email);
 	sanitizer.escape(email);
-	res.send(email);
+	res.send({"message":"true", "email":email});
 });
+
+function authenticate (userObj){
+  return true;
+}
 
 app.listen(port, function(){
 	console.log("App is running on "+ port);
