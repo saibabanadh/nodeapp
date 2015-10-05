e94779d10c84d8e4525ff3d0d4ac23bb08466ba6
SaiNadh <saibaba.nadh@dreamorbit.com>
2015-10-05 13:33:59 +0530

    added helmet and sanitizer

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var port = process.env.PORT || 3000;
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	helmet  = require('helmet'),
+	sanitizer = require('sanitizer');
+
+var app = express(),
+	port = process.env.PORT || 3000;
 
 app.use(express.static('.'));
+
+app.use(helmet.csp({
+  defaultSrc: ["'self'"],
+  scriptSrc: ['*.google-analytics.com'],
+  styleSrc: ["'unsafe-inline'"],
+  imgSrc: ['*.google-analytics.com'],
+  connectSrc: ["'none'"],
+  fontSrc: [],
+  objectSrc: [],
+  mediaSrc: [],
+  frameSrc: []
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
 app.get('/', function(req,res){
 	res.redirect('index.html');
+});
+
+app.post('/auth/validEmail', function(req,res){
+
+	var email = sanitizer.sanitize(req.body.email);
+	res.send(email);
 });
 
 app.listen(port, function(){
